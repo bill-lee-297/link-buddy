f02b29c3b7541ff44f7b0523347853792935acc1
이국진 <billlee297@gmail.com>
2023-05-23 23:14:35 +0900

    feat: 최초 로그인일 경우 회원가입 처리

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,3 +1,5 @@
+import { db } from '@vercel/postgres';
+
 type OAuthUser = {
     id: string;
     name?: string;
@@ -6,14 +8,14 @@ type OAuthUser = {
 };
 
 export default async function addUser(user: OAuthUser) {
-    console.log('add User ', user);
+    const client = await db.connect();
 
-    const data = await fetch('/api/user', {
-        method: 'GET'
-    });
-    console.log(data);
+    const result = await client.sql`SELECT * FROM users WHERE user_email = ${user.email};`;
 
-    console.log('addUser :: ', data);
+    if (result.rows.length > 0) {
+        return true;
+    }
+    await client.sql`INSERT INTO users (user_name, user_email, user_id) VALUES (${user.name}, ${user.email}, ${user.id})`;
 
-    return data;
+    return true;
 }
