fcd1aa1370d12ffa611701f1a3dbe41ddca1105a
kukjin <kukjin@blockodyssey.io>
2024-05-13 15:05:40 +0900

    feat: 모달창에서 esc, enter 동작하도록 작업

--- src/components/AddBookmarkForm.tsx
+++ src/components/AddBookmarkForm.tsx
@@ -11,12 +11,17 @@ type Props = {
 
 export default function AddBookmarkForm({ onClose, link }: Props) {
     const nameElement = useRef(null);
+    const submitBtnElement = useRef(null);
     const [inputLink, setInputLink] = useState(link);
     const [name, setName] = useState('');
-    const [faviconImag_, setFaviconImage] = useState('');
+    const [faviconImag, setFaviconImage] = useState('');
+    const [nameLoading, setNameLoading] = useState(false);
+    const [submitLoading, setSubmitLoading] = useState(false);
 
     const getCrawlingData = async (type: 'name' | 'favicon') => {
-        console.log('exec');
+        if (type === 'name') {
+            setNameLoading(true);
+        }
         const result = await API({
             endpoint: '/api/crawling',
             method: 'GET',
@@ -25,11 +30,10 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
             }
         });
 
-        console.log(result);
-
         if (result.status === 200) {
             if (type === 'name') {
                 setName(result.data.title);
+                setNameLoading(false);
                 return result.data.title;
             }
             if (type === 'favicon') {
@@ -41,7 +45,6 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
     };
 
     const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log('handleUrlChange');
         setInputLink(e.target.value);
         setFaviconImage('');
     };
@@ -55,6 +58,8 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
             alert('북마크의 이름을 입력해주세요.');
             return;
         }
+        setSubmitLoading(true);
+
         const favicon = await getCrawlingData('favicon');
 
         const responseData = await API({
@@ -68,9 +73,22 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
             }
         });
 
+        setSubmitLoading(false);
+
         if (responseData.status === 200) {
             alert('북마크가 추가되었습니다.');
             onClose();
+        }
+    };
+
+    const keyModalClose = (e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            onClose();
+        }
+        if (e.key === 'Enter') {
+            if (!nameLoading && !submitLoading) {
+                submitBtnElement.current?.click();
+            }
         }
     };
 
@@ -80,6 +98,9 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
         }
 
         getCrawlingData('name');
+
+        window.addEventListener('keydown', keyModalClose);
+        return () => window.removeEventListener('keydown', keyModalClose);
     }, []);
 
     return (
@@ -88,20 +109,45 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
             onClick={(e) => e.target === e.currentTarget && onClose()}
             role="none"
         >
-            <div className="flex w-600 flex-col rounded-2xl bg-white p-4">
-                <h1 className="text-xl">북마크 등록</h1>
-                <div className="mt-6 flex flex-col gap-8">
+            <div className="display relative flex w-600 flex-col rounded-2xl bg-white">
+                <div className="p-4">
+                    <h1 className="text-xl">북마크 등록</h1>
+                </div>
+                <div className="border-b-2 border-b-gray-200 " />
+                <div className="flex flex-col gap-8 px-4 py-6">
                     <div className="flex flex-row items-center justify-start">
                         <div className="w-24">이름</div>
-                        <input
-                            type="text"
-                            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                            placeholder="여기에 이름을 입력해주세요."
-                            ref={nameElement}
-                            onChange={(e) => setName(e.target.value)}
-                            value={name}
-                            required
-                        />
+                        {nameLoading ? (
+                            <div role="status">
+                                <svg
+                                    aria-hidden="true"
+                                    className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                                    viewBox="0 0 100 101"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                        fill="currentColor"
+                                    />
+                                    <path
+                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                        fill="currentFill"
+                                    />
+                                </svg>
+                                <span className="sr-only">Loading...</span>
+                            </div>
+                        ) : (
+                            <input
+                                type="text"
+                                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                                placeholder="여기에 이름을 입력해주세요."
+                                ref={nameElement}
+                                onChange={(e) => setName(e.target.value)}
+                                value={name}
+                                required
+                            />
+                        )}
                     </div>
                     <div className="flex flex-row items-center justify-start">
                         <div className="w-24">폴더 선택</div>
@@ -126,7 +172,7 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
                         />
                     </div>
                 </div>
-                <div className="mt-4 flex flex-row items-center justify-end gap-4">
+                <div className="flex flex-row items-center justify-end gap-4 px-4 py-6">
                     <button
                         type="button"
                         className="rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200"
@@ -135,6 +181,7 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
                         취소
                     </button>
                     <button
+                        ref={submitBtnElement}
                         type="button"
                         className="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
                         onClick={() => handleSubmit()}
@@ -142,6 +189,28 @@ export default function AddBookmarkForm({ onClose, link }: Props) {
                         등록
                     </button>
                 </div>
+                {submitLoading && (
+                    <div role="status" className="absolute h-full w-full rounded-2xl bg-gray-500 opacity-50 ">
+                        <div className="absolute left-1/2 top-2/4 -translate-x-1/2 -translate-y-1/2">
+                            <svg
+                                aria-hidden="true"
+                                className="h-12 w-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                                viewBox="0 0 100 101"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor"
+                                />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                )}
             </div>
         </section>
     );
