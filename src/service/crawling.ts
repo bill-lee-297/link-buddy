d9e58741fb7bd32ea8d9d75af218d97a590709d4
kukjin <kukjin@blockodyssey.io>
2024-05-13 22:11:23 +0900

    mod: 구글 favicon 얻을 수 있도록 추가 수정

--- src/service/crawling.ts
+++ src/service/crawling.ts
@@ -6,13 +6,20 @@ import { isValidUrl } from '@/lib/utils';
 const crawling = async (inputLink: string) => {
     const responseData = await axios.get(inputLink);
 
+    // console.log(responseData);
+
     const { protocol } = responseData.request;
     const hostURL = responseData.request.host || inputLink;
     const $ = cheerio.load(responseData.data);
     const title = $('head title').html();
+
+    console.log(responseData.data);
     let favicon = $('link[rel="icon"]').attr('href');
     if (!favicon) {
         favicon = $('link[rel="shortcut icon"]').attr('href');
+    }
+    if (!favicon) {
+        favicon = $('meta[itemprop="image"]').attr('content');
     }
 
     let faviconURL;
