5cb4549694dda5aadc807ceda69cfb03c1ed0eaa
kukjin <kukjin@blockodyssey.io>
2024-05-16 10:38:30 +0900

    mod: 첫 접근 화면 개선

--- src/components/BookmarksArea.tsx
+++ src/components/BookmarksArea.tsx
@@ -9,8 +9,20 @@ export default async function BookmarksArea() {
             <div className="flex h-[400px] w-full flex-col items-center justify-center text-center">
                 링크버디는 하나의 계정으로 북마크를 관리할 수 있습니다. <br />
                 브라우저 마다 관리하기 힘들었던 북마크를 한 곳에서 관리해보세요. <br />
-                <br />
-                <a href="/auth/signin">로그인 하기</a>
+                <div className="mt-10 flex flex-row items-center justify-center gap-4">
+                    <a
+                        href="https://kukjin.notion.site/LinkBuddy-aa990d424ad545df8439a0d2c969294c?pvs=4"
+                        className="mb-2 me-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                    >
+                        링크버디란?
+                    </a>
+                    <a
+                        href="/auth/signin"
+                        className="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        링크버디 시작하기
+                    </a>
+                </div>
             </div>
         );
     }
